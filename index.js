a6eb0d28787c7c2cb0604b71638c4a4e33537b0e
Golik Andrii <Golik.Andrii@gmail.com>
2023-05-09 12:57:39 +0200

    Add Task 2

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+//   --------------TASK 1---------------
 let arrName = ["Семен", "Иван", "Петр", "Татьяна"];
 let arrAge = [18, 27, 74, 34];
 
 for (let i = 0; i <arrName.length; i++){
-    console.log(arrName[i] +" " + arrAge[i] + "лет/годов")
+    let newArr = [arrName[i] +" " + arrAge[i] + " лет/годов"]
+    console.log(newArr)
 }
+console.log('=======================')
+//   --------------TASK 2---------------
+for (let i = arrName.length-1; i >= 0; i--){
+    let newArr = [arrName[i] +" " + arrAge[i] + " лет/годов"]
+    console.log(newArr)
+}
+
